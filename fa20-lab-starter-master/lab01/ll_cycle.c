97ce5bd8a7bfc0d5322ba7c838a24b3bd5b0c593
MicroGrey <wupeiqin@sjtu.edu.cn>
2025-01-22 14:20:02 +0800

    实现链表循环检测功能

--- fa20-lab-starter-master/lab01/ll_cycle.c
+++ fa20-lab-starter-master/lab01/ll_cycle.c
@@ -3,5 +3,14 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    while (head->next != NULL)
+    {
+        if (head->value == 1)
+        {
+            return 1;
+        }
+        head->value=1;
+        head=head->next;
+    }
     return 0;
 }
