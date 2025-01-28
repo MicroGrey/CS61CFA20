9925906b1285e9d592933986c1ff44f2a324c49a
MicroGrey <wupeiqin@sjtu.edu.cn>
2025-01-28 17:28:17 +0800

    实现LFSR计算功能，更新寄存器状态以支持位移和反馈逻辑

--- fa20-lab-starter-master/lab02/lfsr.c
+++ fa20-lab-starter-master/lab02/lfsr.c
@@ -6,5 +6,7 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    uint16_t temp = *reg;
+    * reg = temp>>1 | ( (temp<<15 & 1<<15) ^ (temp<<13 & 1<<15) ^ (temp<<12 & 1<<15) ^ (temp<<10 & 1<<15) );
 }
 
