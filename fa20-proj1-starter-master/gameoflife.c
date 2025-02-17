4248f59ee90d0a9388b90dae217832db6c870fad
MicroGrey <wupeiqin@sjtu.edu.cn>
2025-02-17 17:04:45 +0800

    finish proj1

--- fa20-proj1-starter-master/gameoflife.c
+++ fa20-proj1-starter-master/gameoflife.c
@@ -22,6 +22,95 @@
 //and the left column as adjacent to the right column.
 Color *evaluateOneCell(Image *image, int row, int col, uint32_t rule)
 {
+	Color *new_color = (Color *)malloc(sizeof(Color));
+	new_color->R = 0;
+	new_color->G = 0;
+	new_color->B = 0;
+
+	Color *current = &image->image[row][col];
+	
+	int left = (col - 1 + image->cols) % image->cols;
+	int right = (col + 1) % image->cols;
+	int up = (row - 1 + image->rows) % image->rows;
+	int down = (row + 1) % image->rows;
+
+	for (int i = 0; i < 8; i++)
+	{
+		int red = (int)((image->image[up][left].R >> i) & 1) +
+				 	   ((image->image[up][col].R >> i) & 1) + 
+					   ((image->image[up][right].R >> i) & 1) + 
+					   ((image->image[row][left].R >> i) & 1) + 
+					   ((image->image[row][right].R >> i) & 1) +
+					   ((image->image[down][left].R >> i) & 1) + 
+					   ((image->image[down][col].R >> i) & 1) + 
+					   ((image->image[down][right].R >> i) & 1);
+
+		int green = (int)((image->image[up][left].G >> i) & 1) + 
+						 ((image->image[up][col].G >> i) & 1) + 
+						 ((image->image[up][right].G >> i) & 1) + 
+						 ((image->image[row][left].G >> i) & 1) + 
+						 ((image->image[row][right].G >> i) & 1) + 
+						 ((image->image[down][left].G >> i) & 1) + 
+						 ((image->image[down][col].G >> i) & 1) + 
+						 ((image->image[down][right].G >> i) & 1);
+
+		int blue = (int)((image->image[up][left].B >> i) & 1) + 
+						((image->image[up][col].B >> i) & 1) + 
+						((image->image[up][right].B >> i) & 1) + 
+						((image->image[row][left].B >> i) & 1) + 
+						((image->image[row][right].B >> i) & 1) + 
+						((image->image[down][left].B >> i) & 1) + 
+						((image->image[down][col].B >> i) & 1) + 
+						((image->image[down][right].B >> i) & 1);
+
+		if (current->R >>i & 1)
+		{
+			if ((rule >> (red + 9)) & 1)
+			{
+				new_color->R |= (1 << i);
+			}
+		}
+		else
+		{
+			if ((rule >> (red)) & 1)
+			{
+				new_color->R |= (1 << i);
+			}
+		}
+
+		if (current->G >>i & 1)
+		{
+			if ((rule >> (green + 9)) & 1)
+			{
+				new_color->G |= (1 << i);
+			}
+		}
+		else
+		{
+			if ((rule >> (green)) & 1)
+			{
+				new_color->G |= (1 << i);
+			}
+		}
+
+		if (current->B >>i & 1)
+		{
+			if ((rule >> (blue + 9)) & 1)
+			{
+				new_color->B |= (1 << i);
+			}
+		}
+		else
+		{
+			if ((rule >> (blue)) & 1)
+			{
+				new_color->B |= (1 << i);
+			}
+		}
+
+	}
+	
+	return new_color;
 	//YOUR CODE HERE
 }
 
@@ -29,6 +118,21 @@ Color *evaluateOneCell(Image *image, int row, int col, uint32_t rule)
 //You should be able to copy most of this from steganography.c
 Image *life(Image *image, uint32_t rule)
 {
+	Image *newImage = malloc(sizeof(Image));
+	newImage->image = (Color **)malloc(image->rows * sizeof(Color *));
+	for (int i = 0; i < image->rows; i++)
+	{
+		newImage->image[i] = malloc(image->cols * sizeof(Color));
+		for (int j = 0; j < image->cols; j++)
+		{
+			Color *newColor = evaluateOneCell(image, i, j, rule);
+			newImage->image[i][j] = *newColor;
+			free(newColor);
+		}
+	}
+	newImage->rows = image->rows;
+	newImage->cols = image->cols;
+	return newImage;
 	//YOUR CODE HERE
 }
 
@@ -50,4 +154,38 @@ You may find it useful to copy the code from steganography.c, to start.
 int main(int argc, char **argv)
 {
 	//YOUR CODE HERE
+	if (argc != 3) {
+        printf("usage: ./gameOfLife filename rule\n\
+    			filename is an ASCII PPM file (type P3) with maximum value 255.\n\
+    			rule is a hex number beginning with 0x; Life is 0x1808.");
+        exit(-1);
+    }
+
+    // Read the original image data
+    Image *original_image = readData(argv[1]);
+
+	// turn string into int
+	char *endptr; // for strtol
+	uint32_t rule = (uint32_t) strtol(argv[2], &endptr, 0); // A base of 0 means that the base will be determined by the format of the string (e.g., "0x" for hex, "0" for octal).
+    if (*endptr != '\0') {
+        exit(-1); // the rule is illegal
+    }
+	
+	if (original_image == NULL) {
+        fprintf(stderr, "Failed to read data from %s\n", argv[1]);
+        exit(-1);
+    }
+
+    // Perform steganography on the image
+    Image *new_image = life(original_image, rule);
+    if (new_image == NULL) {
+        fprintf(stderr, "Steganography processing failed\n");
+        freeImage(original_image); // Ensure the original image is freed
+        exit(-1);
+    }
+	writeData(new_image);
+    // Free the image memory
+    freeImage(original_image);
+    freeImage(new_image);
+	return 0;
 }
