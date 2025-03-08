b6c82bb28437d25ddab22b965dfc8076ce9adb93
MicroGrey <wupeiqin@sjtu.edu.cn>
2025-03-09 01:22:10 +0800

    finish lab09

--- fa20-lab-starter-master/lab09/simd.c
+++ fa20-lab-starter-master/lab09/simd.c
@@ -53,8 +53,22 @@ long long int sum_simd(int vals[NUM_ELEMS]) {
 	
 	for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
 		/* YOUR CODE GOES HERE */
+		__m128i chunk, mask, result_chunk;
+		for (unsigned int i = 0; i < NUM_ELEMS / 4 * 4; i += 4) {
+			chunk = _mm_loadu_si128((__m128i *)(vals + i));
+			mask = _mm_cmpgt_epi32(chunk, _127);
+			result_chunk = _mm_and_si128(chunk, mask);
+			result_chunk = _mm_hadd_epi32(result_chunk, result_chunk);
+			result_chunk = _mm_hadd_epi32(result_chunk, result_chunk);
+			result += _mm_cvtsi128_si32(result_chunk);
+		}
 
 		/* You'll need a tail case. */
+		for (unsigned int i = NUM_ELEMS / 4 * 4; i < NUM_ELEMS; i++) {
+			if (vals[i] >= 128) {
+				result += vals[i];
+			}
+		}
 
 	}
 	clock_t end = clock();
@@ -69,8 +83,43 @@ long long int sum_simd_unrolled(int vals[NUM_ELEMS]) {
 	for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
 		/* COPY AND PASTE YOUR sum_simd() HERE */
 		/* MODIFY IT BY UNROLLING IT */
+		__m128i chunk, mask, result_chunk;
+		for (unsigned int i = 0; i < NUM_ELEMS / 16 * 16; i += 16) {
+			chunk = _mm_loadu_si128((__m128i *)(vals + i));
+			mask = _mm_cmpgt_epi32(chunk, _127);
+			result_chunk = _mm_and_si128(chunk, mask);
+			result_chunk = _mm_hadd_epi32(result_chunk, result_chunk);
+			result_chunk = _mm_hadd_epi32(result_chunk, result_chunk);
+			result += _mm_cvtsi128_si32(result_chunk);
+
+			chunk = _mm_loadu_si128((__m128i *)(vals + i + 4));
+			mask = _mm_cmpgt_epi32(chunk, _127);
+			result_chunk = _mm_and_si128(chunk, mask);
+			result_chunk = _mm_hadd_epi32(result_chunk, result_chunk);
+			result_chunk = _mm_hadd_epi32(result_chunk, result_chunk);
+			result += _mm_cvtsi128_si32(result_chunk);
+
+			chunk = _mm_loadu_si128((__m128i *)(vals + i + 8));
+			mask = _mm_cmpgt_epi32(chunk, _127);
+			result_chunk = _mm_and_si128(chunk, mask);
+			result_chunk = _mm_hadd_epi32(result_chunk, result_chunk);
+			result_chunk = _mm_hadd_epi32(result_chunk, result_chunk);
+			result += _mm_cvtsi128_si32(result_chunk);
+
+			chunk = _mm_loadu_si128((__m128i *)(vals + i + 12));
+			mask = _mm_cmpgt_epi32(chunk, _127);
+			result_chunk = _mm_and_si128(chunk, mask);
+			result_chunk = _mm_hadd_epi32(result_chunk, result_chunk);
+			result_chunk = _mm_hadd_epi32(result_chunk, result_chunk);
+			result += _mm_cvtsi128_si32(result_chunk);
+		}
 
 		/* You'll need 1 or maybe 2 tail cases here. */
+		for (unsigned int i = NUM_ELEMS / 16 * 16; i < NUM_ELEMS; i++) {
+			if (vals[i] >= 128) {
+				result += vals[i];
+			}
+		}
 
 	}
 	clock_t end = clock();
